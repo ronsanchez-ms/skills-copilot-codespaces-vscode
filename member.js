906c36e6b838410d9e7a0db01d2edc949c225004
ronsanchez-ms <111525515+ronsanchez-ms@users.noreply.github.com>
2023-07-22 21:28:13 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var about = document.getElementById("about");
+    var contact = document.getElementById("contact");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    about.style.display = "none";
+    contact.style.display = "none";
+}
